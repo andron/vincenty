1e7877419a9bc57317bc0e8f6da1fbdbae1798b8
Anders Ro <anders.ronnbrant@gmail.com>
2012-10-30 22:40:45 +0100

    Remove explicit assignment for source files
    Mostly for testing bob feature with implicit source files.

--- makerules.mk
+++ makerules.mk
@@ -8,10 +8,8 @@ RELEASE  := 1
 REQUIRES :=
 $(call setup_requires)
 
-TARGETS := libvincenty.so
-
 _CXXFLAGS := -march=native
 
-libvincenty.so_SRCS := $(call getsource,src,*.cpp)
+TARGETS := libvincenty.so
 
 include $(FOOTER)
